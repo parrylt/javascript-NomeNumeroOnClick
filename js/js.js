a26e44be230a164aba9115d74683e701d1fc5f7f
Rômulo Junior <132522416+parrylt@users.noreply.github.com>
2023-09-11 00:02:11 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,16 @@
+function exibir () {
+    alert ("Boa noite");
+    var alu = document.getElementById("cxnome").value;
+    var n1 = document.getElementById("cx1").value;
+    document.write("Aluno " + alu + " " + n1);
+}
+
+function cxnomes () {
+    alert ("Dg o nome completo!!");
+}
+
+function vermelho () {
+    document.body.style.backgroundColor = "red";
+}
+
+// CRIAR TRÊS BOTÕES: VERMELHO, VERDE E AZUL para transformar fundo das cores 
